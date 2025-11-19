34597f6808f4e9a1f3e24fde265d0250de717a4e
SuperButler <g00467621@atu.ie>
2025-11-19 00:09:08 +0000

    iot_updates
    adding files for webserver

---
+++ iotCode/webserver/websight.h
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en" > 
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+  <title>Online Security System</title>
+  <style>
+  body { 
+    background-color: Blue;
+  }
+
+  .flex-Container{
+    display: flex;
+    flex-direction: column;
+    background-color: DodgerBlue;
+    align-items: center;
+  }
+
+  h1{
+    font: bold;
+    font-size: 40px;
+    font-family: Arial;
+    color: navy;
+    text-align: center;
+  }
+
+  p{
+    font-size: 25px;
+    font-family: Arial;
+    color: navy;
+    text-align: center;
+  } 
+
+  th, td {
+    font-size: 25px;
+    padding: 8px;
+    text-align: left;
+    2
+    border-bottom: 1px solid #ddd;
+  }
+  </style>
+</head>
+
+<body>
+  <div class="flex-Container">
+    <h1>Online Security System</h1>
+      <p>Distance</p>
+    <table>
+      <tr>
+        <th>Sensor</th>
+        <th>Value</th>
+        <th>Unit</th>
+      </tr>
+      <tr>
+        <td>Temperature</td>
+        <td> 25.4</td>
+        <td>Degree Cel</td>
+      </tr>
+    </table>
+  </div>
+</body>
+</html>
