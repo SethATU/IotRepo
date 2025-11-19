d7d20e299f335c839de0d4b60895c731038ca02e
SuperButler <g00467621@atu.ie>
2025-11-19 20:51:44 +0000

    iot_updates
    was working on rfid but i need to finish keypad first

--- iotMain/iotMain.ino
+++ iotMain/iotMain.ino
@@ -1,12 +1,26 @@
+<<<<<<< Updated upstream
 #include <Wire.h>     //lcd
 #include "rgb_lcd.h"  //lcd
 #include <Keypad.h>   //keypad
 
 rgb_lcd lcd; //lcd
+=======
+#include <Wire.h>      //lcd
+#include "rgb_lcd.h"   //lcd
+#include <Keypad.h>    //keypad
+#include <SPI.h>       //rfid
+#include <MFRC552.h>   //rfid
+//#include <webpage.h> //webpage
+>>>>>>> Stashed changes
 
-#define ROW 4     //keypad
-#define COLUMN 3  //keypad
-#define BUZZ 32   //buzzer
+#define ROW       4  //keypad
+#define COLUMN    3  //keypad
+#define BUZZ      32 //buzzer
+#define SS_RFID   21 //rfid
+#define RDT_RFID  22 //rfid
+#define SCK_RFID  25 //rfid
+#define MOSI_RFID 26 //rfid
+#define MISO_RFID 27 //rfid
 
 int passCode[4] = {1, 2, 3, 4}; //keypad
 int passCodeEntered[4];         //keypad
@@ -19,9 +33,13 @@ char keys[ROW][COLUMN] =  {     //keypad
   {'7', '8', '9'},
   {'*', '0', '#'}
 };
+int alarmStatus = 0;  //alarm
+
+MFRC522 rfid(SS_RFID, RST_RFID);  //rfid
 
 byte pin_rows[ROW] = {2, 0, 4, 16}; //keypad
 byte pin_column[COLUMN] = {17, 5, 18};  //keypad
+byte nuidPICC[4]; //rfid
 
 Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW, COLUMN);  //keypad
 
@@ -31,17 +49,46 @@ void setup()
 
   Wire.begin(19, 23); //lcd
   lcd.begin(16, 2);   //lcd
+  SPI.begin(SCK_RFID, MISO_RFID, MOSI_RFID, SS_RFID); //rfid
 
   pinMode(BUZZ, OUTPUT);  //buzzer 
 
-  lcd.print("Hello ESP32!"); //lcd, test code
-  delay(1000);
-  lcd.clear();
+  rfid.PCD_Init();  
+
+  //lcd.print("Hello ESP32!"); //lcd, test code
+  //delay(1000);
+  //lcd.clear();
 }
 
-void loop() 
-{
+void loop() {
   keyPad();
+<<<<<<< Updated upstream
+=======
+
+  if(alarmStatus == 0) {
+    lcd.clear();
+    lcd.print("Alarm: Unarmed");
+    lcd.setCursor(0, 1);
+    
+  }
+  
+  
+  //alarm status - unarmed 
+  //show status then option to enter code to arm alarm 
+    //correct code - alarm armed 
+    //wrong code - try again
+  //tap card - press * to arm # to cancile 
+
+  //alarm ststus - armed
+  //show status then option to enter code to disarm arm alarm
+    //pin wrong 3x alarm goes off
+    //pin correct alarm unarmed 
+  //tap card to dissarm alarm 
+
+  //alarm status - aremed 
+  //if distance is < 50cm
+    //start 30 second timer and then buzzer goes off 
+>>>>>>> Stashed changes
 }
 
 void keyPad() //keypad function
@@ -76,7 +123,7 @@ void keyPad() //keypad function
   }
 }
 
-void checkCode()  //checks the code is correct 
+void checkCode()  //checks the pass code is correct 
 {
   passCodeWrong = 0;
 
