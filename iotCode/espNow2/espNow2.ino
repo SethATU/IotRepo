b09ff6b1c931a86f7eeea47319f88f6ade83c42a
SuperButler <g00467621@atu.ie>
2025-12-30 20:16:02 +0000

    EspNow
    I have the code working that i can receve information from on esp to anuther however the webserver wontupdate so im working on that next

--- iotCode/espNow2/espNow2.ino
+++ iotCode/espNow2/espNow2.ino
@@ -1,28 +1,93 @@
-/*********
+/*
   Rui Santos & Sara Santos - Random Nerd Tutorials
-  Complete project details at https://RandomNerdTutorials.com/esp-now-many-to-one-esp32/
+  Complete project details at https://RandomNerdTutorials.com/esp32-esp-now-wi-fi-web-server/
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*********/
+*/
 #include <esp_now.h>
+#include <esp_wifi.h>
 #include <WiFi.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 
-// REPLACE WITH THE RECEIVER'S MAC Address
-uint8_t broadcastAddress[] = {0x84, 0x0D, 0x8E, 0xE6, 0x8F, 0xB4};
+// Set your Board ID (ESP32 Sender #1 = BOARD_ID 1, ESP32 Sender #2 = BOARD_ID 2, etc)
+#define BOARD_ID 1
 
-// Structure example to send data
-// Must match the receiver structure
+// Digital pin connected to the DHT sensor
+#define DHTPIN 4  
+
+// Uncomment the type of sensor in use:
+//#define DHTTYPE    DHT11     // DHT 11
+#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+//MAC Address of the receiver 
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+//Structure example to send data
+//Must match the receiver structure
 typedef struct struct_message {
-    int id; // must be unique for each sender board
-    int x;
-    int y;
+    int id;
+    float temp;
+    float hum;
+    int readingId;
 } struct_message;
 
-// Create a struct_message called myData
+esp_now_peer_info_t peerInfo;
+
+//Create a struct_message called myData
 struct_message myData;
 
-// Create peer interface
-esp_now_peer_info_t peerInfo;
+unsigned long previousMillis = 0;   // Stores last time temperature was published
+const long interval = 10000;        // Interval at which to publish sensor readings
+
+unsigned int readingId = 0;
+
+// Insert your SSID
+constexpr char WIFI_SSID[] = "REPLACE_WITH_YOUR_SSID";
+
+int32_t getWiFiChannel(const char *ssid) {
+  if (int32_t n = WiFi.scanNetworks()) {
+      for (uint8_t i=0; i<n; i++) {
+          if (!strcmp(ssid, WiFi.SSID(i).c_str())) {
+              return WiFi.channel(i);
+          }
+      }
+  }
+  return 0;
+}
+
+float readDHTTemperature() {
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  //float t = dht.readTemperature(true);
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return 0;
+  }
+  else {
+    Serial.println(t);
+    return t;
+  }
+}
+
+float readDHTHumidity() {
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  if (isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return 0;
+  }
+  else {
+    Serial.println(h);
+    return h;
+  }
+}
 
 // callback when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
@@ -31,11 +96,21 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 }
  
 void setup() {
-  // Init Serial Monitor
+  //Init Serial Monitor
   Serial.begin(115200);
 
-  // Set device as a Wi-Fi Station
+  dht.begin();
+ 
+  // Set device as a Wi-Fi Station and set channel
   WiFi.mode(WIFI_STA);
+
+  int32_t channel = getWiFiChannel(WIFI_SSID);
+
+  WiFi.printDiag(Serial); // Uncomment to verify channel number before
+  esp_wifi_set_promiscuous(true);
+  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+  esp_wifi_set_promiscuous(false);
+  WiFi.printDiag(Serial); // Uncomment to verify channel change after
 
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
@@ -49,7 +124,6 @@ void setup() {
   
   // Register peer
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
   peerInfo.encrypt = false;
   
   // Add peer        
@@ -60,19 +134,23 @@ void setup() {
 }
  
 void loop() {
-  // Set values to send
-  myData.id = 2;
-  myData.x = random(0,50);
-  myData.y = random(0,50);
-
-  // Send message via ESP-NOW
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-   
-  if (result == ESP_OK) {
-    Serial.println("Sent with success");
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time a new reading was published
+    previousMillis = currentMillis;
+    //Set values to send
+    myData.id = BOARD_ID;
+    myData.temp = readDHTTemperature();
+    myData.hum = readDHTHumidity();
+    myData.readingId = readingId++;
+     
+    //Send message via ESP-NOW
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+    if (result == ESP_OK) {
+      Serial.println("Sent with success");
+    }
+    else {
+      Serial.println("Error sending the data");
+    }
   }
-  else {
-    Serial.println("Error sending the data");
-  }
-  delay(10000);
 }
--- iotMain2.0/iotModual1/iotModual1.ino
+++ iotMain2.0/iotModual1/iotModual1.ino
@@ -1,9 +1,99 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <esp_now.h>
+#include <esp_wifi.h>
+#include <WiFi.h>
 
+const int TRIG = 14;
+const int ECHO = 34;
+long duration;          
+float distance;    
+unsigned long previousMillis = 0;
+const long interval = 10000;     
+
+uint8_t broadcastAddress[] = {0x84, 0x0D, 0x8E, 0xE6, 0x8F, 0xB4};
+
+typedef struct struct_message {
+  float dist;
+} struct_message;
+
+esp_now_peer_info_t peerInfo;
+
+struct_message myData;
+
+constexpr char WIFI_SSID[] = "Backup";
+
+int32_t getWiFiChannel(const char *ssid) {
+  if (int32_t n = WiFi.scanNetworks()) {
+      for (uint8_t i=0; i<n; i++) {
+          if (!strcmp(ssid, WiFi.SSID(i).c_str())) {
+              return WiFi.channel(i);
+          }
+      }
+  }
+  return 0;
+}
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+
+  int32_t channel = getWiFiChannel(WIFI_SSID);
+
+  WiFi.printDiag(Serial); 
+  esp_wifi_set_promiscuous(true);
+  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+  esp_wifi_set_promiscuous(false);
+  WiFi.printDiag(Serial); 
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_send_cb(esp_now_send_cb_t(OnDataSent));
+
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    
+    myData.dist = distanceRead();
 
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+    if (result == ESP_OK) {
+      Serial.println("Sent with success");
+    }
+    else {
+      Serial.println("Error sending the data");
+    }
+  }
+}
+
+float distanceRead() {
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(5);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+
+  duration = pulseIn(ECHO, HIGH, 20000); 
+  distance = (duration * 0.0343) / 2; 
+
+  return distance;
 }
